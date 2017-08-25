8120068ce817828be64306eba6a3186390e15897
marwaalalawy <marwa.alalawy@gmail.com>
2017-08-25 21:36:07 +0300

    the server is runing by npm start and the front-end login page is kind of done

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 var express = require('express');
 var app = express();
 var mongojs = require('mongojs');
@@ -7,27 +9,30 @@ var bodyParser = require('body-parser');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
-// app.get('/contactlist', function (req, res) {
-//   console.log('I received a GET request');
 
-//   db.contactlist.find(function (err, docs) {
-//     console.log(docs);
-//     res.json(docs);
-//   });
-// });
-
-
-
-
-
-// app.get('/contactlist/:id', function (req, res) {
-//   var id = req.params.id;
-//   console.log(id);
-//   db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
-//     res.json(doc);
-//   });
-// });
-
+passport.use(new LocalStrategy({
+    usernameField: 'email'
+  },
+  function(username, password, done) {
+    User.findOne({ email: username }, function (err, user) {
+      if (err) { return done(err); }
+      // Return if user not found in database
+      if (!user) {
+        return done(null, false, {
+          message: 'User not found'
+        });
+      }
+      // Return if password is wrong
+      if (!user.validPassword(password)) {
+        return done(null, false, {
+          message: 'Password is wrong'
+        });
+      }
+      // If credentials are correct, return the user object
+      return done(null, user);
+    });
+  }
+));
 
 app.listen(3000);
 console.log("Server running on port 3000");
